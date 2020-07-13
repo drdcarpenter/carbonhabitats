5cb5f3e0d5c975ebc938778f3680d99034f4662e
Dan Carpenter <drdcarpenter@googlemail.com>
2020-07-13 21:41:18 +0100

    add data

--- calc_soil_carbon.R
+++ calc_soil_carbon.R
@@ -3,6 +3,9 @@ library(sf)
 library(exactextractr)
 
 # read in raster and reproject to crs = 27700
+
+
+newproj <- "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs"
 soil <- raster("result.tiff")
 soil2 <- projectRaster(soil, crs = newproj)
 
