0448ca807f095b0c1dfd51818976a6e238ad2ca7
Dan Carpenter <drdcarpenter@googlemail.com>
2020-09-01 14:18:18 +0100

    remove old code

--- filter_phabs.R
+++
@@ -1,9 +0,0 @@
-library(sf)
-library(dplyr)
-
-phabs <- st_read("G:/Data/Data to use/Habitats/BBO/GIS data/Current/Oxfordshire and Berkshire Habitat and Land Use December 2019.shp")
-
-phabs <- filter(phabs, !S41Habitat %in% c("None", "Not assessed yet")) 
-phabs <- filter(phabs, !is.na(S41Habitat))
-
-unique(phabs$S41Habitat)
